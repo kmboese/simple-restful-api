4fa05b3a8c81b21bd6e130f6ba2f5511595b77ac
Kevin Boese <kevinboese5@gmail.com>
2019-11-17 10:31:27 -0800

    Created User class for User API resource.

--- user.py
+++ user.py
@@ -1,90 +1,36 @@
-from flask_restful import Api, Resource, reqparse
-from http_codes import *
-from messages import *
+DEFAULT_AGE = 35
+DEFAULT_USER = 'DefaultUser'
+DEFAULT_OCCUPATION = 'DefaultOccupation'
 
-# Create a list of local users (actually get this info from a database)
-users = {
-    'Nicholas': {
-        'name': 'Nicholas',
-        'age': 42,
-        'occupation': 'Network Engineer'
-    },
-    'Elvin': {
-        'name': 'Elvin',
-        'age': 32,
-        'occupation': 'Doctor'
-    },
-    'Jass': {
-        'name': 'Jass',
-        'age': 22,
-        'occupation': 'Web Developer'
-    }
-}
-
-# RESTful endpoint to get user data
-class User(Resource):
-    def get(self, name):
-
-        # Attempt to retrieve user
-        if users.get(name):
-            return users[name], HTTP_OK
-            
-        # Handle user not found
+class User():
+    def __init__(self, name=None, age=None, occupation=None):
+        if name:
+            self.name = name
         else:
-            return USER_NOT_FOUND_MSG, HTTP_NOT_FOUND
-
-    def post(self, name):
-        global users
-
-        parser = reqparse.RequestParser()
-        parser.add_argument('age')
-        parser.add_argument('occupation')
-        args = parser.parse_args()
-        
-        # Check if user already exists
-        if users.get(name) is not None:
-            return 'User with name {} already exists'\
-                .format(name), HTTP_BAD_REQUEST
-
-        # Otherwise, create the user
-        user = {
-            'name': name,
-            'age': args['age'],
-            'occupation': args['occupation']
-        }
-        key = user['name']
-        users[key] = user
-        return user, HTTP_CREATED
-
-    def put(self, name):
-        global users 
-
-        parser = reqparse.RequestParser()
-        parser.add_argument('age')
-        parser.add_argument('occupation')
-        args = parser.parse_args()
-
-        # If user doesn't exist, create the user
-        user = {
-                'name': name,
-                'age': args['age'],
-                'occupation': args['occupation']
-            }
-        if users.get(name) is None:
-            key = user['name']
-            users[key] = user
-            return user, HTTP_CREATED
-        
-        # Else, update the user
-        users[name] = user
-        return user, HTTP_OK
-
-    def delete(self, name):
-        global users
-
-        if users.get(name):
-            users.pop(name)
-            return '{} has been deleted.'.format(name), HTTP_OK
-
+            self.name = DEFAULT_USER
+        if age:
+            self.age = age
         else:
-            return USER_NOT_FOUND_MSG, HTTP_NOT_FOUND
+            self.age = DEFAULT_AGE
+        if occupation:
+            self.occupation = occupation
+        else:
+            self.occupation = DEFAULT_OCCUPATION
+
+    def __str__(self):
+        ret = 'User:\n'
+        ret += 'Name: {}\n'.format(self.name)
+        ret += 'Age: {}\n'.format(self.age)
+        ret += 'Occupation: {}\n'.format(self.occupation)
+
+        return ret
+        
+### Helper Functions ###
+
+# Get URL endpoint given a User object
+def getUserEndpoint(user):
+    endpoint = str(user.name) + '?'
+    endpoint += 'age=' + str(user.age)
+    endpoint += '&occupation=' + str(user.occupation)
+
+    return endpoint
