7e144d1abd846142bb9618ecc1d6a22d5a2e66b0
Kevin Boese <kevinboese5@gmail.com>
2019-11-17 10:31:43 -0800

    Added "put" support to command line arguments

--- client.py
+++ client.py
@@ -2,21 +2,26 @@ import requests
 import urllib3
 
 from http_codes import isError
+from user import User, getUserEndpoint
 
 EXIT = 'exit'
+
 
 # Prompt the user for an HTTP Request type
 def printActionTypePrompt():
     print('Please enter an action (available actions: get, put, post, delete)'
     ' or \'exit\' to end the program:')
 
+
 # Prompt the user for a user name
 def printUserNamePrompt():
     print('Please enter a user name:')
 
+
 # Goodbye message printed when program ends
 def printGoodbyeMessage():
     print('Goodbye!')
+
 
 # Get action type from user
 def getActionType():
@@ -39,22 +44,94 @@ def getResourceName():
     resourceInput = input('> ')
     return resourceInput
 
-# Get user from server
-def getUser(url, name):
+
+# Get User from server by name and print response
+def printUser(url, name):
     resource = 'user/' + name
     request = '/'.join([url, resource])
 
     try:
         r = requests.get(request)
 
-        print('Request: {}\nReponse: {}HTTP Code: {}'
-            .format(request, r.text, r.status_code))
+        print('Response: {}\nHTTP Code: {}'
+            .format(r.text, r.status_code))
 
     # Handle failures
     except ConnectionError as e:
         raise ConnectionError
 
+
+# Get User from server by name and return User object
+def getUser(url, name):
+    resource = 'user/' + name
+    request = '/'.join([url, resource])
+    user = User()
+
+    try:
+        r = requests.get(request)
+        userData = r.json()
+
+        if isError(r.status_code):
+            print('Error retrieving user {}: returned code {}'
+                .format(name, r.status_code))
+            return None
+        else:
+            user.name = userData['name']
+            user.age = userData['age']
+            user.occupation = userData['occupation']
+            return user
+
+    except ConnectionError:
+        raise ConnectionError
+
+
+# Update user 
+def updateUser(url, name):
+    field = -1
+    newName, newAge, newOccupation = None, None, None
+
+    # Prompt user for field to change
+    while field < 1 or field > 3:
+        print('Which field do you want to update for this user?')
+        print('1.\tName')
+        print('2.\tAge')
+        print('3.\tOccupation')
+
+        field = int(input('> '))
     
+    if field == 1:
+        newName = input('Enter a new name for this user > ')
+    elif field == 2:
+        newAge = input('Enter a new age for this user > ')
+    elif field ==3 :
+        newOccupation = input('Enter a new occupation for this user > ')
+    
+    try:
+        # Get user from server
+        user = getUser(url, name)
+
+        # Update user data locally
+        if newName:
+            user.name = newName
+        if newAge:
+            user.age = newAge
+        if newOccupation:
+            user.occupation = newOccupation
+
+        # Send PUT request using new User data
+        resource = 'user/' + getUserEndpoint(user)
+        request = '/'.join([url, resource])
+        r = requests.put(request)
+
+        # Show results
+        print('Request: {}'.format(request))
+        printUser(url, name)
+
+    # Handle failures
+    except ConnectionError:
+        raise ConnectionError
+
+
 
 def main():
     localhostIP = '127.0.0.1'
@@ -81,8 +158,11 @@ def main():
         # Handle API request
         try:
             if actionInput == 'get':
-                getUser(url, resourceInput)
+                printUser(url, resourceInput)
+            elif actionInput == 'put':
+                updateUser(url, resourceInput)
                 
+
         except ConnectionError as e:
             print(e)
             exit(1)
